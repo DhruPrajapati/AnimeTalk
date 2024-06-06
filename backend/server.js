136c760461d2cf582ded02464692fdcd4c013bae
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-06-06 17:09:56 -0400

    backend completed but socket part is pending

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import auth from "./routes/auth.js";
+import message from "./routes/message.js";
+import user from "./routes/user.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,7 +13,12 @@ const PORT = process.env.PORT || 5020;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", auth);
+app.use("/api/messages", message);
+app.use("/api/users", user);
+
 
 app.listen(PORT, () => {
   connectToMongoDB();
