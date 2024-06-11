8c4f447817790fa3585e4ee992b71e18afce8888
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-06-11 19:16:22 -0400

    edit package.json

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,18 +9,23 @@ import user from "./routes/user.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-
 const PORT = process.env.PORT || 5020;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use("/api/auth", auth);
 app.use("/api/messages", message);
 app.use("/api/users", user);
-
 
 server.listen(PORT, () => {
   connectToMongoDB();
