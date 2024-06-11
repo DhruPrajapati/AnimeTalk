174682b3ad05032078e7909ad82371857aec458b
DhruPrajapati <76686616+DhruPrajapati@users.noreply.github.com>
2024-06-11 19:38:11 -0400

    url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5020", {
+      const socket = io("https://animetalk.onrender.com", {
         query: {
           userId: authUser._id,
         },
